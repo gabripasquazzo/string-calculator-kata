2cf43761eb38144d41ad7912fd429bdbc56bb94d
gabriele pasquazzo <gpasquazzo@gmail.com>
2021-10-14 22:45:09 +0200

    step 1

--- src/kata.js
+++ src/kata.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const add = (numbers) =>{
+    if ( numbers === undefined || numbers === "" )
+        return 0;
+    if ( numbers.indexOf(',') > 0 ) {
+        let values = numbers.split(',');
+        return parseInt(values[0])+parseInt(values[1]);
+    }
+    return parseInt(numbers);
+}
+
+exports.add = add;
--- test/kata.test.js
+++ test/kata.test.js
@@ -1,5 +1,14 @@
 'use strict'
+const kata = require('../src/kata')
 
-test('scaffold test', () =>{
-    expect(true).toBe(true);
+test('empty input', () => {
+    expect(kata.add("")).toBe(0);
+});
+
+test('one input: "1"', () => {
+    expect(kata.add("1")).toBe(1);
+});
+
+test('base input: "2,1"', () => {
+    expect(kata.add("2,1")).toBe(3);
 });
