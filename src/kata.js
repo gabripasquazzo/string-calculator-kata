ccbf768830e1c94cb0e9e2261a258b76882d7d1f
gabriele pasquazzo <gpasquazzo@gmail.com>
2021-10-14 22:54:01 +0200

    add linter

--- src/kata.js
+++ src/kata.js
@@ -1,13 +1,14 @@
-'use strict'
+'use strict';
 
 const add = (numbers) =>{
-    if ( numbers === undefined || numbers === "" )
-        return 0;
-    if ( numbers.indexOf(',') > 0 ) {
-        let values = numbers.split(',');
-        return parseInt(values[0])+parseInt(values[1]);
-    }
-    return parseInt(numbers);
-}
+  if ( numbers === undefined || numbers === '' ) {
+    return 0;
+  }
+  if ( numbers.indexOf(',') > 0 ) {
+    const values = numbers.split(',');
+    return parseInt(values[0])+parseInt(values[1]);
+  }
+  return parseInt(numbers);
+};
 
 exports.add = add;
--- test/kata.test.js
+++ test/kata.test.js
@@ -1,14 +1,14 @@
-'use strict'
-const kata = require('../src/kata')
+'use strict';
+const kata = require('../src/kata');
 
 test('empty input', () => {
-    expect(kata.add("")).toBe(0);
+  expect(kata.add('')).toBe(0);
 });
 
 test('one input: "1"', () => {
-    expect(kata.add("1")).toBe(1);
+  expect(kata.add('1')).toBe(1);
 });
 
 test('base input: "2,1"', () => {
-    expect(kata.add("2,1")).toBe(3);
+  expect(kata.add('2,1')).toBe(3);
 });
